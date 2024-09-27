392e1ba8e3d1ca672ac12e70bf8c58bce16815b1
ahmed <kgahmed90@gmail.com>
2024-09-27 10:50:22 +0400

    testing file

---
+++ TESTING.py
@@ -0,0 +1,22 @@
+# a = 33 
+# b = 200 
+# if not a > b:
+#     print("a is NOT greater than b")
+
+
+# age = 26
+
+# if age < 30:
+#     print("your age is less than 30 years")
+#     if age > 25:
+#         print("your age is already greater than 25")
+#     else:
+#         print("your age is greater than 30 yooo")
+
+
+# a = 50
+# b = 10
+
+# if a == b:
+#     print("1")
+    
